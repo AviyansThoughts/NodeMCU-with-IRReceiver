1351548361a1a00052fea69b703bc29243af029b
Aviyan's Thoughts <93308974+AviyansThoughts@users.noreply.github.com>
2025-03-01 20:10:22 +0530

    Code

---
+++ IR_Receiver.ino
@@ -0,0 +1,55 @@
+// Welcome to Aviyan's Thoughts
+
+#include <Arduino.h>
+#define DECODE_NEC          // Includes Apple and Onkyo. To enable all protocols , just comment/disable this line.
+#include "PinDefinitionsAndMore.h"
+#include <IRremote.hpp> // include the library
+int led = D2;
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(led, OUTPUT);
+    while (!Serial)
+        ; // Wait for Serial to become available. Is optimized away for some cores.
+
+    // Just to know which program is running on my Arduino
+    Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
+
+    // Start the receiver and if not 3. parameter specified, take LED_BUILTIN pin from the internal boards definition as default feedback LED
+    IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK); // D5 pin
+
+    Serial.print(F("Ready to receive IR signals of protocols: "));
+    printActiveIRProtocols(&Serial);
+    Serial.println(F("at pin " STR(IR_RECEIVE_PIN)));
+}
+
+void loop() {
+    if (IrReceiver.decode()) {
+
+        /*
+         * Print a summary of received data
+         */
+        if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
+            Serial.println(F("Received noise or an unknown (or not yet enabled) protocol"));
+            // We have an unknown protocol here, print extended info
+            IrReceiver.printIRResultRawFormatted(&Serial, true);
+            IrReceiver.resume(); // Do it here, to preserve raw data for printing with printIRResultRawFormatted()
+        } else {
+            IrReceiver.resume(); // Early enable receiving of the next IR frame
+            IrReceiver.printIRResultShort(&Serial);
+            IrReceiver.printIRSendUsage(&Serial);
+        }
+        Serial.println();
+
+        /*
+         * Finally, check the received data and perform actions according to the received command
+         */
+        if (IrReceiver.decodedIRData.command == 0xA) {
+            // do something
+            digitalWrite(led, HIGH);
+       } else if (IrReceiver.decodedIRData.command == 0x1B) {
+            // do something else
+            digitalWrite(led, LOW);
+        }
+    }
+}
